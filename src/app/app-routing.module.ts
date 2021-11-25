7d79be57f332fba4502365c42f6b6a68cb0b5115
muhammad.monir <muhammad.monir@cognizant.com>
2021-11-24 18:27:34 -0600

    login component set up completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  {path: 'welcome', component: WelcomeComponent}
+  {path: '', component: WelcomeComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'login', component: LoginComponent},
+
+  {path: '**', component: ErrorComponent}
 ];
 
 @NgModule({
